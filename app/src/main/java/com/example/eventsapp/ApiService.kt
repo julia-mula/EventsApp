f268b92186df08369fb35488884b0f51e7743d34
jakub-mrow <jakub@mrowczynski.net>
2023-05-27 13:00:39 +0200

    feat: not finished logging

--- app/src/main/java/com/example/eventsapp/ApiService.kt
+++ app/src/main/java/com/example/eventsapp/ApiService.kt
@@ -3,6 +3,7 @@ package com.example.eventsapp
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.Headers
 import retrofit2.http.POST
 import retrofit2.http.Path
 
@@ -13,5 +14,9 @@ interface ApiService {
 
     @POST("/users/{userId}/events")
     fun createUserEvent(@Path("userId") userId: Int, @Body event: NewEvent): Call<Event>
+
+    @Headers("Content-Type: application/json")
+    @POST("/login")
+    fun loginUser(@Body loginBody: LoginJson): Call<LoginResponse>
 
 }
--- app/src/main/java/com/example/eventsapp/fragments/LoginFragment.kt
+++ app/src/main/java/com/example/eventsapp/fragments/LoginFragment.kt
@@ -1,13 +1,29 @@
 package com.example.eventsapp.fragments
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
+import com.example.eventsapp.ApiService
+import com.example.eventsapp.Event
+import com.example.eventsapp.LoginJson
+import com.example.eventsapp.LoginResponse
 import com.example.eventsapp.R
 import com.example.eventsapp.databinding.FragmentLoginBinding
+import com.google.gson.Gson
+import okhttp3.MediaType
+import okhttp3.Request
+import okhttp3.RequestBody
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 
 class LoginFragment : Fragment() {
@@ -28,7 +44,41 @@ class LoginFragment : Fragment() {
         binding = FragmentLoginBinding.inflate(inflater, container, false)
 
         binding.proceedButton.setOnClickListener {
+            val retrofit: Retrofit = Retrofit.Builder().baseUrl("http://18.185.8.100/")
+                .addConverterFactory(GsonConverterFactory.create()).build()
+
+            val api: ApiService = retrofit.create(ApiService::class.java)
+
+            val username: String = binding.editTextUsername.text.toString()
+            val password: String = binding.editTextPassword.text.toString()
+            val loginData = LoginJson(username=username, password=password)
+            Log.d("DATA", "$username, $password")
+
             findNavController().navigate(R.id.action_loginFragment_to_dashboardFragment)
+
+            //TODO login endpoint
+//            val loginCall = api.loginUser(loginData)
+//
+//            loginCall.enqueue(object: Callback<LoginResponse> {
+//                @SuppressLint("NotifyDataSetChanged")
+//                override fun onResponse(call: Call<LoginResponse>, response: Response<LoginResponse>) {
+//                    Log.d("DATA", response.toString())
+//                    if (response.isSuccessful){
+//                        val loginResponse = response.body()
+//                        findNavController().navigate(R.id.action_loginFragment_to_dashboardFragment)
+//
+//                    } else if (response.code() == 401) {
+//                        Toast.makeText(context, "Invalid username or password", Toast.LENGTH_SHORT).show()
+//                        findNavController().navigate(R.id.action_loginFragment_to_dashboardFragment)
+//                    }
+//                }
+//
+//                override fun onFailure(call: Call<LoginResponse>, t: Throwable) {
+//                    Toast.makeText(context, "Incorrect username or password", Toast.LENGTH_SHORT).show()
+//                    t.printStackTrace()
+//                }
+//
+//            })
         }
 
         return binding.root
